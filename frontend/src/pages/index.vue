1b3051c518b5d0c05ebf7d7968b79770a3fb0c34
Moritz Oczko <moritz89@icloud.com>
2024-11-06 22:20:49 +0100

    add API-test

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -2,10 +2,10 @@
   <h1 class="text-center mt-5 text-pink">Frontend ... under construction</h1>
   <div class="d-flex justify-center mt-5 ga-5">
     <v-btn class="text-dark" to="../testroute">testRoute#</v-btn>
-    <v-btn class="bg-pink">Backend check</v-btn>
+    <v-btn @click="testBackend" class="bg-pink">Backend check</v-btn>
   </div>
   <div v-if="showConfirm" class="d-flex justify-center">
-    <h3></h3>
+    <h3>{{ backEndAnswer }}</h3>
     <v-icon
       class="mt-6"
       color="success"
@@ -18,4 +18,20 @@
 
 <script setup>
 const showConfirm = ref(false);
+const backEndAnswer = ref("");
+const testBackend = async () => {
+  const url = "https://dummyjson.com/products";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    backEndAnswer.value = json;
+    showConfirm.value = !showConfirm.value;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 </script>
