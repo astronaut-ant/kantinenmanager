a88277a19f4f249d3ae06dca49aa4c851a9597ee
Moritz Oczko <moritz89@icloud.com>
2024-11-06 22:09:09 +0100

    clean up frontend

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <HelloWorld />
+  <h1 class="text-center mt-5 text-pink">Frontend ... under construction</h1>
+  <div class="d-flex justify-center mt-5 ga-5">
+    <v-btn class="text-dark" to="../testroute">testRoute#</v-btn>
+    <v-btn class="bg-pink">Backend check</v-btn>
+  </div>
+  <div v-if="showConfirm" class="d-flex justify-center">
+    <h3></h3>
+    <v-icon
+      class="mt-6"
+      color="success"
+      icon="mdi-check-circle-outline"
+      size="128"
+    >
+    </v-icon>
+  </div>
 </template>
 
 <script setup>
-  //
+const showConfirm = ref(false);
 </script>
