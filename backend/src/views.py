673231efa880e0db0eb0819bb6c3473f7171bf0b
Lennox Schuster <fx26ifam@studserv.uni-leipzig.de>
2024-11-07 08:01:49 +0100

    Install swagger for API documentation

--- backend/src/views.py
+++ backend/src/views.py
@@ -1,6 +1,19 @@
 from . import app
+from flask import redirect
+
+
+@app.route("/")
+def index():
+    return redirect("/health", code=302)
 
 
 @app.route("/health")
 def health():
+    """Example endpoint returning OK if the service is healthy.
+    This is an example of a health check endpoint.
+    ---
+    responses:
+      200:
+        description: OK
+    """
     return "OK"
