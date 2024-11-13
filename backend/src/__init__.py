3bd5853ce0b174ae5b34292928cafff361ca1dd7
Sidney Stiller <yi10ihas@uni-leipzig.de>
2024-11-13 16:24:50 +0100

    initialize database ref #2

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flasgger import Swagger
 from flask_cors import CORS
+from .database import init_db
+
 
 app = Flask(__name__)
 CORS(app)
 swagger = Swagger(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://postgres:miau@db:5432/postgres"
+init_db(app)
