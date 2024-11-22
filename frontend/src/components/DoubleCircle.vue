c876fe69ec0b0f3ee694c23bf61d9e93e2e2b379
Moritz Oczko <moritz89@icloud.com>
2024-11-22 01:29:39 +0100

    add QR-Reader

---
+++ frontend/src/components/DoubleCircle.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-sheet
+    class="rounded-circle me-n5 opacity-70"
+    :class="formattedColor"
+    height="100"
+    width="100"
+  ></v-sheet>
+  <v-sheet
+    class="rounded-circle ms-n5 bg-green opacity-70"
+    height="100"
+    width="100"
+  ></v-sheet>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["color"]);
+const formattedColor = computed(() => {
+  return "bg-" + props.color;
+});
+</script>
--- frontend/src/pages/Kuechenpersonal/KuecheQR.vue
+++ frontend/src/pages/Kuechenpersonal/KuecheQR.vue
@@ -1,6 +1,47 @@
 <template>
   <NavbarKueche />
-  <h1>QR-Scanner</h1>
+  <div class="d-flex h-100 justify-center align-center mt-n10">
+    <div class="w-50 h-50 auto">
+      <qrcode-stream
+        class="pa-1"
+        :paused="!inScanMode"
+        @detect="onDetect"
+        :track="trackQR"
+      >
+        <div
+          class="position-absolute w-100 h-100 border-primary rounded elevation-2"
+        >
+          <ScannedOrder
+            v-if="!inScanMode"
+            @close="inScanMode = !inScanMode"
+            :data="qrMessage"
+          />
+        </div>
+      </qrcode-stream>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+const inScanMode = ref(true);
+
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
+const qrMessage = ref({});
+
+const onDetect = (detectedCode) => {
+  qrMessage.value = detectedCode;
+  inScanMode.value = !inScanMode.value;
+};
+
+const trackQR = (detectedCodes, ctx) => {
+  for (const detectedCode of detectedCodes) {
+    const {
+      boundingBox: { x, y, width, height },
+    } = detectedCode;
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#F44336";
+    ctx.strokeRect(x, y, width, height);
+  }
+};
+</script>
