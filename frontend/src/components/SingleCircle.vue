cb0b17158d92ea684c663e54e179c43b782ece0c
Moritz Oczko <moritz89@icloud.com>
2024-11-24 19:01:13 +0100

    edit SingleCircle.vue

---
+++ frontend/src/components/SingleCircle.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-sheet
+    class="rounded-circle opacity-70"
+    :class="formattedColor"
+    height="100"
+    width="100"
+  ></v-sheet>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["color"]);
+const formattedColor = computed(() => {
+  return "bg-" + props.color;
+});
+</script>
