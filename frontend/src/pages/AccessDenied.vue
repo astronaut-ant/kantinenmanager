eab7f77d0bf7730d36bd802d26e8a4b74bf95a2d
Moritz Oczko <moritz89@icloud.com>
2024-11-24 18:29:00 +0100

    edit AccessDeniedPage.vue

---
+++ frontend/src/pages/AccessDenied.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="d-flex h-100 flex-column justify-center">
+    <h1 class="text-red text-center">
+      <v-icon class="me-5">mdi-hand-back-left</v-icon>Zugang verweigert!
+    </h1>
+  </div>
+</template>
