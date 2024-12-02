9fae916c9fa9238ab98b7531c755a691ddcb58a7
Nico <pq04ihuh@uni-leipzig.de>
2024-12-02 15:13:17 +0100

    create Employee manuell

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,7 +8,16 @@ import VerwaltungBehinderung from "@/pages/Verwaltung/VerwaltungBehinderung.vue"
 import KuecheUebersicht from "@/pages/Kuechenpersonal/KuecheUebersicht.vue";
 import KuecheQR from "@/pages/Kuechenpersonal/KuecheQR.vue";
 import AccessDenied from "@/pages/AccessDenied.vue";
-import VerwaltungCsvUpload from "@/pages/Verwaltung/VerwaltungCsvUpload.vue";
+import VerwaltungNeuerMitarbeiterCsvUpload from "@/pages/Verwaltung/VerwaltungNeuerMitarbeiterCsvUpload.vue";
+import axios from "axios";
+import Gruppenleitung from "@/pages/Gruppenleitung/Gruppenleitung.vue";
+import Standortleitung from "@/pages/Standortleitung/Standortleitung.vue";
+import VerwaltungAlleMitarbeiter from "@/pages/Verwaltung/VerwaltungAlleMitarbeiter.vue";
+import VerwaltungAlleStandorte from "@/pages/Verwaltung/VerwaltungAlleStandorte.vue";
+import VerwaltungNeuerStandort from "@/pages/Verwaltung/VerwaltungNeuerStandort.vue";
+import VerwaltungAlleGruppen from "@/pages/Verwaltung/VerwaltungAlleGruppen.vue";
+import VerwaltungNeueGruppe from "@/pages/Verwaltung/VerwaltungNeueGruppe.vue";
+import VerwaltungMitarbeiterManuell from "@/pages/Verwaltung/VerwaltungMitarbeiterManuell.vue";
 
 const routes = [
   { path: "/", component: index, redirect: "/login" },
@@ -36,10 +45,69 @@ const routes = [
     },
   },
   {
-    path: "/verwaltung/behinderung/csv-upload",
-    component: VerwaltungCsvUpload,
-    beforeEnter: (ton, from, next) => {
-      protectRoute(next);
+    path: "/verwaltung/mitarbeiter/neuerMitarbeiter",
+    component: VerwaltungNeuerMitarbeiter,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "verwaltung");
+    },
+  },
+  {
+    path: "/verwaltung/mitarbeiter/neuerMitarbeiterCsvUpload",
+    component: VerwaltungNeuerMitarbeiterCsvUpload,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "verwaltung");
+    },
+  },
+  {
+    path: "/verwaltung/mitarbeiter/neuerMitarbeiterManuell",
+    component: VerwaltungMitarbeiterManuell,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "verwaltung");
+    },
+  },
+  {
+    path: "/verwaltung/standorte/uebersicht",
+    component: VerwaltungAlleStandorte,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "verwaltung");
+    },
+  },
+  {
+    path: "/verwaltung/standorte/neuerStandort",
+    component: VerwaltungNeuerStandort,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "verwaltung");
+    },
+  },
+
+  {
+    path: "/verwaltung/gruppen/uebersicht",
+    component: VerwaltungAlleGruppen,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "verwaltung");
+    },
+  },
+
+  {
+    path: "/verwaltung/gruppen/neueGruppe",
+    component: VerwaltungNeueGruppe,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "verwaltung");
+    },
+  },
+
+  {
+    path: "/gruppenleitung/uebersicht",
+    component: Gruppenleitung,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "gruppenleitung");
+    },
+  },
+  {
+    path: "/standortleitung/uebersicht",
+    component: Standortleitung,
+    beforeEnter: (to, from, next) => {
+      protectRoute(next, "standortleitung");
     },
   },
   {
