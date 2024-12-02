5550d114d50a3e8db5a63cd9d2fb8f85d4276941
Hazem <hazemrik@gmail.com>
2024-12-02 18:12:27 +0100

    Fixed bug with search, added transition

--- frontend/src/pages/Verwaltung/VerwaltungAlleMitarbeiter.vue
+++ frontend/src/pages/Verwaltung/VerwaltungAlleMitarbeiter.vue
@@ -1,16 +1,17 @@
 <template>
   <NavbarVerwaltung />
   <v-container max-width="1000">
-   <div>
-      <div>
-        <v-toolbar color="white" flat dark>
-          <p class="text-h5 font-weight-black" >Übersicht Mitarbeiter</p>
-          <v-spacer></v-spacer>
-          <v-btn icon="mdi-magnify" @click="toggleSearchField"></v-btn>
-          <v-btn icon="mdi-reload"></v-btn>
-        </v-toolbar>
-      </div>
-      <v-text-field
+    <div>
+      <v-toolbar color="white" flat dark>
+        <p class="text-h5 font-weight-black" >Übersicht Mitarbeiter</p>
+        <v-spacer></v-spacer>
+        <v-btn icon="mdi-magnify" @click="toggleSearchField"></v-btn>
+        <v-btn icon="mdi-reload"></v-btn>
+      </v-toolbar>
+    </div>
+    <div class="d-flex justify-center">
+      <v-expand-transition>
+        <v-text-field
         v-if="isSearchVisible"
         v-model="search"
         density="compact"
@@ -21,14 +22,19 @@
         hide-details
         single-line
         clearable
-      ></v-text-field>
+        rounded
+        max-width="800"
+        ></v-text-field>
+      </v-expand-transition>
+    </div>
+    <div>
       <v-data-table :headers="headers"  :items="items" :search="search">
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-btn icon="mdi-lead-pencil" class="bg-primary mr-2" @click="openDialog(item)" size="small"></v-btn>
-          <v-btn icon="mdi-trash-can-outline" class="bg-red" @click="openDialog(item)" size="small"></v-btn>
-        </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-btn icon="mdi-lead-pencil" class="bg-primary mr-2" @click="openDialog(item)" size="small"></v-btn>
+        <v-btn icon="mdi-trash-can-outline" class="bg-red" @click="openDialog(item)" size="small"></v-btn>
+      </template>
       </v-data-table>
-   </div>
+    </div>
   </v-container>
 </template>
  
