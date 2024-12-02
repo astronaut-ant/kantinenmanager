1f94bff0aac0abc569eff10001374aaf09ebc4f0
Hazem <hazemrik@gmail.com>
2024-12-03 00:24:28 +0100

    Added QR Code button; Added Delete Dialog

--- frontend/src/pages/Verwaltung/VerwaltungAlleMitarbeiter.vue
+++ frontend/src/pages/Verwaltung/VerwaltungAlleMitarbeiter.vue
@@ -30,24 +30,54 @@
     <div>
       <v-data-table :headers="headers"  :items="items" :search="search">
       <template v-slot:[`item.actions`]="{ item }">
+        <v-btn icon="mdi-qrcode" class="bg-green mr-2" @click="openDialog(item)" size="small"></v-btn>
         <v-btn icon="mdi-lead-pencil" class="bg-primary mr-2" @click="openDialog(item)" size="small"></v-btn>
-        <v-btn icon="mdi-trash-can-outline" class="bg-red" @click="openDialog(item)" size="small"></v-btn>
+        <v-btn icon="mdi-trash-can-outline" class="bg-red" @click="opendeleteDialog(item)" size="small"></v-btn>
       </template>
       </v-data-table>
     </div>
   </v-container>
+  <v-dialog v-model="deleteDialog" persistent max-width="400">
+    <v-card>
+      <v-card-text>
+        <div class="d-flex justify-center text-red mb-4">
+          <p class="text-h5 font-weight-black" >Mitarbeiter löschen</p>
+        </div>
+        <div class="text-medium-emphasis">
+          <p> Sind Sie sicher, dass Sie den Mitarbeiter <strong>{{ employeeToDelete }}</strong> löschen möchten?</p>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text @click="closedeleteDialog">Abbrechen</v-btn>
+        <v-btn color="red" variant="elevated" @click="confirmDelete">Löschen</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
  
  
 <script setup>
   const search = ref("");
   const isSearchVisible = ref(false);
+  const deleteDialog = ref(false);
+  const employeeToDelete = ref("");
   const toggleSearchField = () => {
     if (isSearchVisible.value) {
       search.value = "";
     }
     isSearchVisible.value = !isSearchVisible.value;
   };
+
+  const opendeleteDialog = (item) => {
+    employeeToDelete.value = item.lastname + ", " + item.firstname;
+    deleteDialog.value= true;
+  };
+
+  const closedeleteDialog = () => {
+    deleteDialog.value = false;
+  };
+
+
   const items = ref([
      {
          employee_number: 0,
