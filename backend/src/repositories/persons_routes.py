8abfe2997a749c8b16e4999e668ed5b7a5c472ca
Amelie <139980455+ammyyy2002@users.noreply.github.com>
2024-12-04 16:31:59 +0100

    added persons_routes and persons_service

---
+++ backend/src/repositories/persons_routes.py
@@ -0,0 +1,48 @@
+from uuid import UUID
+from src.utils.auth_utils import login_required
+from src.utils.error import ErrMsg, abort_with_err
+from src.models.user import UserGroup
+from flask import Blueprint
+from flasgger import swag_from
+from src.services.persons_service import PersonsService
+
+persons_routes = Blueprint("persons_routes", __name__)
+
+
+@persons_routes.get("/api/persons/create-qr-code/<uuid:person_id>")
+@login_required(groups=[UserGroup.verwaltung])
+@swag_from(
+    {
+        "tags": ["persons"],
+        "parameters": [
+            {
+                "in": "path",
+                "name": "person_id",
+                "required": True,
+                "schema": {"type": "string"},
+            }
+        ],
+        "responses": {
+            200: {
+                "description": "Successfully created QR code as a PDF for the person",
+                "content": {
+                    "application/pdf": {
+                        "schema": {"type": "string", "format": "binary"}
+                    }
+                },
+            },
+            404: {
+                "description": "QR code could not be created for the person",
+            },
+        },
+    }
+)
+def create_qr_code(person_id: UUID):
+    """Create the QR code for a person by ID
+    Create the QR code for a person by ID
+
+    Authentication: required
+    Authorization: Verwaltung
+    ---
+    """
+    return PersonsService.create_qr_code(person_id)
