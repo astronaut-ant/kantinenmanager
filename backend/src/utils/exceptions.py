8e4b80ae5371e6301107b893f38cca45d62b5ffd
khaoneowie <lalitakuhn@icloud.com>
2024-12-04 20:38:46 +0100

    added GroupDoesNotExistError to exceptions.py

--- backend/src/utils/exceptions.py
+++ backend/src/utils/exceptions.py
@@ -1,4 +1,14 @@
+from uuid import UUID
+
+
 class LocationAlreadyExistsError(Exception):
     """Exception raised when a location with the same name already exists."""
 
     pass
+
+
+class GroupDoesNotExistError(Exception):
+    """Exception raised when a group does not exist at a given location."""
+
+    def __init__(self, group_id: UUID):
+        super().__init__(f"Die Gruppe mit der ID {group_id} existiert nicht.")
