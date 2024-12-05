04d421e51562e52b2b3a77028f6aa47bf04105bd
Moritz Oczko <moritz89@icloud.com>
2024-12-05 03:27:09 +0100

    add calender

--- frontend/src/pages/Gruppenleitung/GruppenleitungVorbestellungen.vue
+++ frontend/src/pages/Gruppenleitung/GruppenleitungVorbestellungen.vue
@@ -1,6 +1,47 @@
 <template>
   <NavbarGruppenleitung />
-  <h1>Vorbestellungen</h1>
+  <h1 class="text-center mt-5 mb-5">Vorbestellungen</h1>
+  <div class="ps-10 pe-10">
+    <FullCalendar :options="calendarOptions" />
+    <CalendarDialog :show="showDialog"></CalendarDialog>
+  </div>
 </template>
 
-<script setup></script>
+<script>
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import CalendarDialog from "@/components/CalendarDialog.vue";
+
+export default {
+  components: {
+    FullCalendar, // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      showDialog: false,
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: "dayGridMonth",
+        dateClick: this.handleDateClick,
+        events: [
+          { title: "Gruppe 1", date: "2024-12-07" },
+          {
+            title: "Gruppe 2 (Vertretung)",
+            date: "2024-12-07",
+            backgroundColor: "hotpink",
+            borderColor: "white",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    handleDateClick: function (arg) {
+      //alert("date click! " + arg.dateStr);
+      this.showDialog;
+      this.calendarOptions.events.push({ title: "Gruppe1", date: arg.dateStr });
+    },
+  },
+};
+</script>
