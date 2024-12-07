47119ac6d1a3762d71293c1ae8de0f9caab281cb
Moritz Oczko <moritz89@icloud.com>
2024-12-07 14:31:04 +0100

    Add calendar range

--- frontend/src/pages/Gruppenleitung/GruppenleitungVorbestellungen.vue
+++ frontend/src/pages/Gruppenleitung/GruppenleitungVorbestellungen.vue
@@ -26,6 +26,25 @@ import interactionPlugin from "@fullcalendar/interaction";
 import CalendarDialog from "@/components/CalendarDialog.vue";
 import Bestellformular from "@/components/Bestellformular.vue";
 
+const calcCalendarEdges = () => {
+  const range = 14;
+  const hourOfOrderStop = 8;
+  const startDate = new Date();
+  const endDate = new Date(startDate);
+  //added 1 for including the endDate itself
+  endDate.setDate(endDate.getDate() + range + 1);
+  const startTime = startDate.getHours();
+  //no order possible after hour of orderstop
+  // if (startTime >= hourOfOrderStop) {
+  //   startDate.setDate(startDate.getDate() + 1);
+  // }
+  const startDateIso = startDate.toISOString().split("T")[0];
+  const endDateIso = endDate.toISOString().split("T")[0];
+  return { start: startDateIso, end: endDateIso };
+};
+
+const edges = calcCalendarEdges();
+
 export default {
   components: {
     FullCalendar, // make the <FullCalendar> tag available
@@ -39,7 +58,18 @@ export default {
         plugins: [dayGridPlugin, interactionPlugin],
         initialView: "dayGridMonth",
         dateClick: this.handleDateClick,
-        events: [],
+        validRange: {
+          start: edges.start,
+          end: edges.end,
+        },
+        events: [
+          {
+            start: "2024-12-21",
+            end: "2024-12-21",
+            color: "red",
+            display: "background",
+          },
+        ],
       },
     };
   },
