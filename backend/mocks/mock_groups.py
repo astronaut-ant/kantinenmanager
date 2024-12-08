78b7d8835b3f1da707428d741fefeac64b854332
Lennox Schuster <fx26ifam@studserv.uni-leipzig.de>
2024-12-08 19:54:31 +0000

    feat: add mock data for database

---
+++ backend/mocks/mock_groups.py
@@ -0,0 +1,38 @@
+MOCK_GROUPS = [
+    {
+        "group_name": "klassik",
+        "location_name": "earth",
+    },
+    {
+        "group_name": "film",
+        "location_name": "earth",
+    },
+    {
+        "group_name": "actors",
+        "location_name": "earth",
+    },
+    {
+        "group_name": "actors",
+        "location_name": "mars",
+    },
+    {
+        "group_name": "comedy",
+        "location_name": "mars",
+    },
+    {
+        "group_name": "science",
+        "location_name": "mars",
+    },
+    {
+        "group_name": "literature",
+        "location_name": "mars",
+    },
+    {
+        "group_name": "crime",
+        "location_name": "mars",
+    },
+    {
+        "group_name": "leaders",
+        "location_name": "earth",
+    },
+]
---
+++ backend/mocks/mock_locations.py
@@ -0,0 +1,8 @@
+MOCK_LOCATIONS = [
+    {
+        "location_name": "earth",
+    },
+    {
+        "location_name": "mars",
+    },
+]
