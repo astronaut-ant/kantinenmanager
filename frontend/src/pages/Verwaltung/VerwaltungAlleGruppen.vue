31be0b6412cb8ff93e5855a9f039029a3b552cd9
Nico <pq04ihuh@uni-leipzig.de>
2024-12-11 15:44:06 +0100

    Gruppen+Standorte

--- frontend/src/pages/Verwaltung/VerwaltungAlleGruppen.vue
+++ frontend/src/pages/Verwaltung/VerwaltungAlleGruppen.vue
@@ -1,3 +1,228 @@
 <template>
+  <NavbarVerwaltung/>
   <h1 class="text-center text-pink">Alle Gruppen</h1>
+  <div class="mx-4 my-5 d-flex justify-start flex-wrap">
+    <v-card class="mx-4 my-4" elevation="16" min-width="17em" max-width="17em"
+      v-for="group in sortedGroups"
+      :key="group.id"
+    >
+      <v-card-item>
+        <div>
+          <v-card-title>
+            <div class="d-flex flex-column">
+              <span class="text-h6 font-weight-bold text-truncate">{{ group.name }}</span>
+              <span class="text-subtitle-2 text-truncate">Standort: {{ group.area }}</span>
+            </div>
+          </v-card-title>
+        </div>
+        <v-card-actions class="justify-end">
+          <v-btn class="mt-2 bg-primary"
+            @click="showDetails(group)"
+          >
+            <v-icon>mdi-information-outline</v-icon>
+          </v-btn>
+          <v-btn class="mt-2 bg-red"
+            @click="handleDelete(group)"
+          >
+            <v-icon>mdi-trash-can-outline</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card-item>
+    </v-card>
+ </div>
+ <v-dialog v-model="detailDialog" persistent max-width="600">
+  <v-card>
+    <v-card-title>
+      <div>
+        <v-icon left class="mr-2">mdi-account-group</v-icon>
+        <span class="text-h5" style="white-space: normal; word-wrap: break-word;">
+          {{ selectedGroup?.name }} - {{ selectedGroup?.area }}
+        </span>
+      </div>
+    </v-card-title>
+    <v-card-text>
+      <p><strong>Gruppenleiter:</strong> {{ selectedGroup?.leiter }}</p>
+      <p><strong>Mitglieder:</strong></p>
+      <v-data-table :headers="headers" :items="selectedGroup?.members">
+      </v-data-table>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary" @click="closeDetailDialog">Schließen</v-btn>
+    </v-card-actions>
+  </v-card>
+ </v-dialog>
+
+ <v-dialog v-model="deleteDialog" persistent max-width="600">
+  <v-card>
+    <v-card-text>
+      <div class="d-flex justify-center text-red mb-4">
+        <p class="text-h5 font-weight-black" >Gruppe löschen</p>
+      </div>
+      <div class="text-medium-emphasis">
+        <p> Sind Sie sicher, dass Sie die Gruppe <strong>{{ groupToDelete?.name }} - {{ groupToDelete?.area }}</strong> löschen möchten?</p>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text @click="closeDeleteDialog">Abbrechen</v-btn>
+      <v-btn color="red" variant="elevated" @click="confirmDelete">Löschen</v-btn>
+    </v-card-actions>
+  </v-card>
+ </v-dialog>
 </template>
+
+<script setup>
+const detailDialog = ref(false);
+const deleteDialog = ref(false);
+const selectedGroup = ref(null);
+const groupToDelete = ref(null);
+
+
+const groups = [
+  {
+    name: "Berufsbildungsbereich 1",
+    area: "W1",
+    id: 9509,
+    leiter: "Hans Friedrich",
+    members: [{firstName: "Allice", lastName: "Skywalker", number: 892484},
+              {firstName: "Bob", lastName: "Baumeister", number: 23},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Hallon", lastName: "Maus", number: 3847},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},
+              {firstName: "Charlie", lastName: "Sjdjnedo", number: 2083},],
+  },
+  {
+    name: "Montage/Verpackung 1",
+    area: "Zedtlitz",
+    id: 1234,
+    leiter: "Petra Müller",
+    members: ["Diana", "Elias", "Felix"],
+  },
+  {
+    name: "FBB 2 - Gruppe 1",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Autoaufbereitung",
+    area: "Zedtlitz",
+    id: 209,
+    leiter: "Hans Friedrich",
+    members: ["Alice", "Bob", "Charlie"],
+  },
+  {
+    name: "Stanzanlage/Spritzgußmaschine",
+    area: "W1kshskdsjdkdsjksjjiopnosrdrddsjdskj",
+    id: 9293,
+    leiter: "Petra Müller",
+    members: ["Diana", "Elias", "Felix"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "Zedtlitz",
+    id: 98765,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+  {
+    name: "Gruppe 3",
+    area: "W1",
+    id: 8472,
+    leiter: "Tom Schulz",
+    members: ["Gina", "Hanna", "Isabel"],
+  },
+];
+
+const headers = [
+  {title: 'Nummer', key: 'number'},
+  {title: 'Vorname', key: 'firstName'},
+  {title: 'Nachname', key: 'lastName'}
+];
+
+const sortedGroups = groups.sort((a, b) => {
+  if (a.area < b.area) {return -1;}
+  if (a.area > b.area) {return 1;}
+
+  if (a.name < b.name) {return -1;}
+  if (a.name > b.name) {return 1;}
+
+  return 0;
+});
+
+const showDetails = (group) => {
+  selectedGroup.value = group;
+  detailDialog.value = true;
+};
+
+const closeDetailDialog = () => {
+  detailDialog.value = false;
+  selectedGroup.value = null;
+}
+
+const handleDelete = (group) => {
+  groupToDelete.value = group;
+  deleteDialog.value = true;
+};
+
+const confirmDelete = () => {
+  closeDeleteDialog();
+}
+
+const closeDeleteDialog = () => {
+  deleteDialog.value = false;
+  groupToDelete.value = null;
+};
+</script>
--- frontend/src/pages/Verwaltung/VerwaltungAlleStandorte.vue
+++ frontend/src/pages/Verwaltung/VerwaltungAlleStandorte.vue
@@ -1,3 +1,146 @@
 <template>
-  <h1 class="text-center text-pink">Alle Standorte</h1>
+  <NavbarVerwaltung/>
+  <div class="mx-4 my-5 d-flex justify-start flex-wrap">
+    <v-card class="mx-4 my-4" elevation="16" min-width="20em" max-width="20em"
+      v-for="location in locations"
+    >
+      <v-card-item>
+        <v-card-title>
+          <div class="d-flex flex-column">
+            <span class="text-h6 font-weight-bold text-truncate">
+              Standort: {{ location?.name }}
+            </span>
+            <span class="text-subtitle-2 text-truncate">
+              Standortleiter: {{ getLocationLeader(location?.locationLeaderID) }}
+            </span>
+          </div>
+        </v-card-title>
+        <v-card-actions class="justify-end">
+          <v-btn class="mt-2 bg-primary"
+            @click="openEditDialog(location)"
+          >
+            <v-icon>mdi-lead-pencil</v-icon>
+          </v-btn>
+          <v-btn class="mt-2 bg-red"
+            @click="handleDelete(location)"
+          >
+            <v-icon>mdi-trash-can-outline</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card-item>
+    </v-card>
+  </div>
+
+  <v-dialog v-model="editDialog" persistent max-width="400">
+    <v-card>
+      <v-card-title class="primary d-flex justify-start">
+        <v-icon left class="mr-2"> mdi-map-marker-radius </v-icon>
+        <span class="text-h5">Standort bearbeiten</span>
+      </v-card-title>
+      <v-card-text>
+        <span>Neue Standortleitung festlegen</span>
+        <v-menu>
+          <template #activator="{ props }">
+            <v-text-field
+              v-bind="props"
+              v-model="newLocationLeaderName"
+              label="Neue Standortleitung"
+              readonly
+              append-inner-icon="mdi-chevron-down"
+            ></v-text-field>
+          </template>
+          <v-list>
+            <v-list-item v-for="leader in locationLeaders"
+              :key="leader.userID"
+              @click="selectLeader(leader)"
+            >
+              <v-list-item-title>{{ leader?.firstName }} {{ leader?.lastName }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text @click="closeEditDialog">Abbrechen</v-btn>
+        <v-btn
+          color="primary"
+          :disabled="!form"
+          type="submit"
+          variant="elevated"
+          @click="confirmEdit"
+          >Speichern
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <v-dialog v-model="deleteDialog" persistent max-width="600">
+  <v-card>
+    <v-card-text>
+      <div class="d-flex justify-center text-red mb-4">
+        <p class="text-h5 font-weight-black" >Standort löschen</p>
+      </div>
+      <div class="text-medium-emphasis">
+        <p> Sind Sie sicher, dass Sie den Standort <strong>{{ locationToDelete?.name }}</strong> löschen möchten?</p>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text @click="closeDeleteDialog">Abbrechen</v-btn>
+      <v-btn color="red" variant="elevated" @click="confirmDelete">Löschen</v-btn>
+    </v-card-actions>
+  </v-card>
+ </v-dialog>
 </template>
+
+<script setup>
+const newLocationLeaderID = ref(null);
+const newLocationLeaderName = ref("");
+const form = ref(false);
+const editDialog = ref(false);
+const locationToEdit = ref(null);
+const deleteDialog = ref(false);
+const locationToDelete = ref(null);
+
+const locations = [{name: "W1", locationID: 293, locationLeaderID: 435},
+  {name: "W8", locationID: 253, locationLeaderID: 1},
+  {name: "W13", locationID: 23, locationLeaderID: 2},
+  {name: "Zedtlitz", locationID: 29, locationLeaderID: 3}];
+
+const locationLeaders = [{firstName: "Hanz", lastName: "Scheibe", userID: 435},
+{firstName: "Franz", lastName: "Weise", userID: 1},
+{firstName: "Glanz", lastName: "Schneise", userID: 2},
+{firstName: "Natan", lastName: "Der Weise", userID: 3}];
+
+const getLocationLeader = (leaderID) => {
+  const leader = locationLeaders.find((l) => l.userID === leaderID);
+  return leader ? `${leader.firstName} ${leader.lastName}` : "Not Found";
+};
+
+const openEditDialog = (location) => {
+  editDialog.value = true;
+  locationToEdit.value = location;
+};
+const closeEditDialog = () => {
+  editDialog.value = false;
+  locationToEdit.value = null;
+  newLocationLeaderID.value = null;
+  form.value = false;
+  newLocationLeaderName.value = "";
+};
+const selectLeader = (newLeader) => {
+  newLocationLeaderID.value = newLeader.userID;
+  newLocationLeaderName.value = newLeader.firstName+" "+newLeader.lastName;
+  form.value = true;
+};
+
+const handleDelete = (location) => {
+  locationToDelete.value = location;
+  deleteDialog.value = true;
+};
+const closeDeleteDialog = () => {
+  locationToDelete.value = null;
+  deleteDialog.value = false;
+};
+const confirmDelete = () => {
+
+};
+
+</script>
