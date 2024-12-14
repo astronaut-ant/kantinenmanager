626b56af328f1934b1c28a0072bc504847c0bd0c
Hazem <hazemrik@gmail.com>
2024-12-14 23:00:22 +0100

    Finished setting and removing replacement

--- frontend/src/components/GroupLeaderCard.vue
+++ frontend/src/components/GroupLeaderCard.vue
@@ -1,41 +1,156 @@
 <template>
-    <v-card class="mx-4 my-4" width="430" elevation="16" min-width="20em" :color="props.anwesend ? 'white' : 'red-lighten-5'">
-        <v-card-text>
-            <div class="d-flex flex-warp justify-space-between mb-4">
-                <div class="text-left ml-1">
-                    <p class="text-h6 font-weight-black text-primary"> {{ props.name }} </p>
-                </div>
+    <v-card class="mx-2 my-2" width="450" elevation="16">
+        <v-card-item>
+            <div class="mb-2 d-flex justify-center">
+                <v-chip
+                    :color="available ? ( replacing_group ? 'orange' : 'green'): 'red'"
+                    class="text-uppercase d-flex justify-center flex-grow-1" 
+                    size="small"
+                    label
+                >
+                    <v-icon class="me-2" v-if="available && !replacing_group">mdi-calendar-check-outline</v-icon>
+                    <v-icon class="me-2" v-else-if="available && replacing_group">mdi-calendar-clock-outline</v-icon>
+                    <v-icon class="me-2" v-else>mdi-calendar-remove-outline</v-icon>
+                    {{ available ? ( replacing_group ? 'Vertretung für ' + props.replacing_group.name : 'verfügbar'): 'abwesend' }}
+                </v-chip>
+            </div>
+            <div class="d-flex align-center">
                 <div>
-                    <v-chip
-                        :color="props.anwesend ? 'green' : 'red'"
-                        :text="props.anwesend ? 'verfügbar' : 'Nicht verfügbar'"
-                        class="text-uppercase"
+                    <v-card-title>{{ props.group_leader.first_name }} {{ props.group_leader.last_name }}</v-card-title>
+                    <v-card-subtitle>
+                        <v-icon
+                        color="primary"
+                        icon="mdi-account-group"
                         size="small"
-                        label>
-                    </v-chip>
+                        ></v-icon>
+                        <span class="me-1 ml-2">Hauptgruppe: {{ props.group_name }}</span>
+                    </v-card-subtitle>
                 </div>
             </div>
+            
+        </v-card-item>
+        <v-card-text>
             <v-divider></v-divider>
-            <div class="d-flex flex-wrap justify-space-between">
-                <div class="mt-5 ml-2 text-medium-emphasis">
-                    <v-chip color="primary" label> {{ props.group }} </v-chip>
-                </div>
-                <div class="mt-5 d-flex ga-1">
-                    <v-btn 
-                    class="mt-2" 
-                    :text="props.anwesend ? 'Als Abwesend markieren' : 'Als Anwesend markieren'"
-                    @click="openDialog"
-                    :color="props.anwesend ? 'red' : 'primary'"
-                    ></v-btn>
-                </div>
+            <div class="mt-3 d-flex justify-end">
+                <v-btn v-if="available" :disabled="replacing_group" color="red" @click="opensetGroupReplacementDialog" size="small" flat variant="outlined">Als Abwesend markieren</v-btn>
+                <v-btn v-else color="primary" @click="openremoveGroupReplacementDialog" size="small" flat variant="outlined">Als Anwesend markieren</v-btn>
             </div>
         </v-card-text>
     </v-card>
+    <v-dialog v-model="setGroupReplacementDialog" persistent max-width="600">
+        <v-card>
+            <v-card-text>
+                <div class="d-flex justify-center text-primary mb-7">
+                <p class="text-h5 font-weight-black" >Vertretung setzen</p>
+                </div>
+                <div class="text-medium-emphasis mb-7">
+                <p> Bitte wählen Sie einen neuen Gruppenleiter, der für die Gruppe <strong>{{ props.group_name }}</strong>  Essensbestellungen übernehmen kann. </p>
+                </div>
+                <v-select
+                    v-model="replacementGroupLeader"
+                    :items="trueavailable_group_leaders.value"
+                    item-title="name"
+                    item-value="id"
+                    label="Gruppenleiter"
+                ></v-select>
+                <v-alert
+                    color="red"
+                    icon="mdi-alert-outline"
+                    text="Der aktuelle Gruppenleiter kann bis zu seiner Rückkehr keine Bestellungen für seine Gruppe vornehmen."
+                    density="compact"
+                ></v-alert>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn text @click="closesetGroupReplacementDialog">Abbrechen</v-btn>
+                <v-btn color="primary" variant="elevated" @click="confirmSetGroupReplacement">Vertretung setzen</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="removeGroupReplacementDialog" persistent max-width="600">
+        <v-card>
+            <v-card-text>
+                <div class="d-flex justify-center text-primary mb-7">
+                <p class="text-h5 font-weight-black" >Vertretung löschen</p>
+                </div>
+                <div class="text-medium-emphasis mb-7">
+                <p> Durch das Entfernen der Vertretung kann <strong>{{ props.group_leader.first_name }} {{ props.group_leader.last_name }}</strong> wieder Essensbestellungen für die Gruppe <strong>{{ props.group_name }}</strong> übernehmen.</p>
+                </div>
+
+            </v-card-text>
+            <v-card-actions>
+                <v-btn text @click="closeremoveGroupReplacementDialog">Abbrechen</v-btn>
+                <v-btn color="primary" variant="elevated" @click="removeGroupReplacement">Vertretung löschen</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+
 </template>
+    
 
 <script setup>
-    const props = defineProps(["id", "name", "loaction" , "group", "anwesend"]);
-    const abwesend = ref(false);
+    const replacementGroupLeader = ref("");
+    const setGroupReplacementDialog = ref(false);
+    const removeGroupReplacementDialog = ref(false);
+    const trueavailable_group_leaders = ref([]);
+    const props = defineProps(["group_id", "group_name", "location" , "group_leader", "group_leader_replacement", "replacing_group", "available", "available_group_leaders"]);
+    const emit = defineEmits(["replacement-set", "replacement-removed"]);
+
+    import axios from "axios";
+
+    trueavailable_group_leaders.value = computed(() => {
+        return props.available_group_leaders.filter(
+            (leader) => leader.id !== props.group_leader.id
+        );
+    });
 
 
+    const removeGroupReplacement = () => {
+        axios
+        .delete(`http://localhost:4200/api/groups/remove-replacement/${props.group_id}`, { withCredentials: true })
+        .then(() => {
+            emit("replacement-removed");
+            closeremoveGroupReplacementDialog();
+        })
+        .catch((err) => console.error("Error deleting", err));
+    };
+    const opensetGroupReplacementDialog = () => {
+        setGroupReplacementDialog.value = true;
+    };
+
+    const closesetGroupReplacementDialog = () => {
+        setGroupReplacementDialog.value = false;
+    };
+
+    const openremoveGroupReplacementDialog = () => {
+        removeGroupReplacementDialog.value = true;
+    };
+
+    const closeremoveGroupReplacementDialog = () => {
+        removeGroupReplacementDialog.value = false;
+    };
+    
+
+    const confirmSetGroupReplacement = () => {
+        if (!replacementGroupLeader) {
+            console.error("No group leader selected");
+            return;
+        }
+        console.log(replacementGroupLeader.value)
+        axios
+            .put(`http://localhost:4200/api/groups/${props.group_id}`, 
+                {   
+                    group_name: props.group_name,
+                    location_id: props.location.id,
+                    user_id_group_leader: props.group_leader.id,
+                    user_id_replacement: replacementGroupLeader.value
+                }, { withCredentials: true }
+            )
+            .then(() => {
+                console.log("Replacement set successfully");
+                emit("replacement-set");
+                closesetGroupReplacementDialog();
+            })
+            .catch((err) => console.error("Error setting replacement", err));
+    };
 </script>
--- frontend/src/pages/Standortleitung/StandortleitungVertretung.vue
+++ frontend/src/pages/Standortleitung/StandortleitungVertretung.vue
@@ -1,36 +1,123 @@
 <template>
   <NavbarStandort></NavbarStandort>
-  <GroupLeaderCard
-    v-for="groupleader in groupleaders"
-    :id="groupleader.id"
-    :name="groupleader.name"
-    :loaction="groupleader.loaction"
-    :group="groupleader.group"
-    :anwesend="groupleader.anwesend">
+  <v-container fluid class="d-flex justify-center">
+    <div class="ml-10 mr-10 mt-5">
+      <v-row class="d-flex justify-start">
+        <v-col
+          v-if="loading"
+          v-for="n in 6"
+          :key="n"
+          cols="12" sm="12" md="6" lg="4" xl="3" xxl = "2"
+          class="d-flex justify-center"
+        >
+          <v-skeleton-loader
+            class="mx-auto"
+            width="400"
+            max-height="200"
+            type="heading, subtitle, divider, chip"
+          />
+        </v-col>
 
-  </GroupLeaderCard>
-  <GroupLeaderCardNew></GroupLeaderCardNew>
+        <v-col
+          v-else
+          v-for="groupLeader in groupLeaders"
+          :key="groupLeader.id"
+          cols="12" sm="12" md="6" lg="4" xl="3" xxl = "2"
+          class="d-flex justify-center"
+        >
+        <GroupLeaderCard
+          :group_id="groupLeader.id"
+          :group_name="groupLeader.group_name"
+          :location="groupLeader.location"
+          :group_leader="groupLeader.group_leader"
+          :group_leader_replacement="groupLeader.group_leader_replacement"
+          :replacing_group="groupLeader.replacing_group"
+          :available="groupLeader.available"
+          :available_group_leaders="groupLeader.available_group_leaders"
+          @replacement-set="fetchData"
+          @replacement-removed="fetchData"
+        ></GroupLeaderCard>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
   
 <script setup>
+  import axios from "axios";
+  const groupLeaders = ref([]);
+  const loading = ref(true);
 
-import GroupLeaderCardNew from '@/components/GroupLeaderCardNew.vue';
+  const fetchData = () => {
+    loading.value=true;
+    groupLeaders.value=[];
+    axios
+      .get("http://localhost:4200/api/groups", { withCredentials: true })
+      .then((response) => {
+        const groups = response.data;  
+        let availableLeaders = []; 
 
-  const groupleaders = [
-    {
-        id: '95f0534f-1580-4d09-8573-8b419d7dc1df',
-        name: 'Herr Müller',
-        loaction: 'W1',
-        group: 'W1',
-        anwesend: false
-    },
-    {
-        id: '95f0534f-1580-4d09-8573-8b419d7dc1df',
-        name: 'Herr Müller',
-        loaction: 'W1',
-        group: 'W1',
-        anwesend: true
-    },
-  ];
+        groups.forEach((group) => {
+          const isLeaderAvailable =
+            !group.group_leader_replacement &&
+            !groups.some(
+              (g) => g.group_leader_replacement?.id === group.group_leader.id
+            );
+
+          if (isLeaderAvailable) {
+            availableLeaders.push({
+              id: group.group_leader.id,
+              name: `${group.group_leader.first_name} ${group.group_leader.last_name}`,
+              group: {
+                id: group.id,
+                name: group.group_name,
+                location: group.location,
+              },
+            });
+          }
+      });
+
+
+        availableLeaders = availableLeaders.filter(
+          (leader, index, self) =>
+            index === self.findIndex((l) => l.id === leader.id)
+        );
+
+
+        groups.forEach((group) => {
+          const replacingGroup = groups.find(
+            (g) => g.group_leader_replacement?.id === group.group_leader.id
+          );
+
+          groupLeaders.value.push({
+            ...group,
+            available: !group.group_leader_replacement, 
+            replacing_group: replacingGroup
+              ? {
+                  id: replacingGroup.id,
+                  name: replacingGroup.group_name,
+                  location: replacingGroup.location,
+                }
+              : null,
+            available_group_leaders: availableLeaders,
+          });
+        });
+
+        groupLeaders.value.sort((a, b) => {
+          if (!a.available && b.available) return -1;
+          if (a.available && !b.available) return 1;
+
+          return a.group_name.localeCompare(b.group_name);
+        });
+
+        console.log(groupLeaders);
+        loading.value = false;
+      })
+      .catch((err) => console.error("Error fetching data", err));
+  };
+
+  onMounted(() => {
+    fetchData();
+  });
 </script>
   
