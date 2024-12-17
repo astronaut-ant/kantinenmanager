9142b763d2581e6f0bd8ac6bb289bdd5d84c56d7
Moritz Oczko <moritz89@icloud.com>
2024-12-17 11:50:23 +0100

    render order from qr

--- frontend/src/components/ScannedOrder.vue
+++ frontend/src/components/ScannedOrder.vue
@@ -7,22 +7,25 @@
     <v-container
       max-height="50"
       min-width="30"
-      class="d-flex justify-center mt-2"
+      class="d-flex justify-center mt-2 mb-10"
     >
-      <DoubleCircle v-if="isHauptgericht1Salat" color="primary" />
-      <DoubleCircle v-if="isHauptgericht2Salat" color="red" />
-      <SingleCircle v-if="isHauptgericht1" color="primary" />
-      <SingleCircle v-if="isHauptgericht2" color="red" />
+      <DoubleCircle v-if="maindishBlue && salad" color="primary" />
+      <DoubleCircle v-else-if="maindishRed && salad" color="red" />
+      <SingleCircle v-else-if="maindishBlue && !salad" color="primary" />
+      <SingleCircle v-else-if="maindishRed && !salad" color="red" />
+      <SingleCircle v-else-if="salad" color="success" />
+      <v-icon v-else color="red" size="15vw">mdi-hand-back-left</v-icon>
     </v-container>
-    <div class="d-flex justify-center">
+    <div class="d-flex justify-center mt-15">
       <v-btn
         width="25vw"
         height="5vw"
-        class="bg-blue-grey mt-10 elevation-10"
+        class="bg-blue-grey elevation-10"
         @click="$emit('close')"
       >
-        <v-icon class="text-xs-h5 text-sm-h4 text-md-h3 text-lg-h2 align-center"
-          >mdi-check</v-icon
+        <v-icon
+          class="text-xs-h5 text-sm-h4 text-md-h3 text-lg-h2 align-center"
+          >{{ accepted ? "mdi-check" : "mdi-close" }}</v-icon
         >
       </v-btn>
     </div>
@@ -32,13 +35,78 @@
 <script setup>
 import DoubleCircle from "./DoubleCircle.vue";
 import SingleCircle from "./SingleCircle.vue";
+import axios from "axios";
 
-const dialog = ref(true);
 const props = defineProps(["data"]);
-const isHauptgericht1Salat =
-  props.data[0].rawValue == "Hauptgericht 1 mit Salat";
-const isHauptgericht2Salat =
-  props.data[0].rawValue == "Hauptgericht 2 mit Salat";
-const isHauptgericht1 = props.data[0].rawValue == "Hauptgericht 1";
-const isHauptgericht2 = props.data[0].rawValue == "Hauptgericht 2";
+const scannedId = props.data[0].rawValue;
+let order;
+
+const maindishRed = ref(false);
+const maindishBlue = ref(false);
+const salad = ref(false);
+const accepted = ref(true);
+
+const redAndGreen = () => {
+  maindishRed.value = true;
+  maindishBlue.value = false;
+  salad.value = true;
+  accepted.value = true;
+};
+const blueAndGreen = () => {
+  maindishRed.value = false;
+  maindishBlue.value = true;
+  salad.value = true;
+  accepted.value = true;
+};
+const red = () => {
+  maindishRed.value = true;
+  maindishBlue.value = false;
+  salad.value = false;
+  accepted.value = true;
+};
+const blue = () => {
+  maindishRed.value = false;
+  maindishBlue.value = true;
+  salad.value = false;
+  accepted.value = true;
+};
+const green = () => {
+  maindishRed.value = false;
+  maindishBlue.value = false;
+  salad.value = true;
+  accepted.value = true;
+};
+const forbidden = () => {
+  maindishRed.value = false;
+  maindishBlue.value = false;
+  salad.value = false;
+  accepted.value = false;
+};
+
+axios
+  .get(`http://localhost:4200/api/daily-orders/person/${scannedId}`, {
+    withCredentials: true,
+  })
+  .then((response) => {
+    order = response.data;
+    console.log(order);
+    if (order.handed_out) {
+      forbidden();
+    } else if (order.salad_option && order.main_dish === "rot") {
+      redAndGreen();
+    } else if (order.salad_option && order.main_dish === "blau") {
+      blueAndGreen();
+    } else if (order.salad_option) {
+      green();
+    } else if (!order.salad_option && order.main_dish === "rot") {
+      red();
+    } else if (!order.salad_option && order.main_dish === "blau") {
+      blue();
+    } else {
+      forbidden();
+    }
+  })
+  .catch((err) => {
+    forbidden();
+  });
 </script>
