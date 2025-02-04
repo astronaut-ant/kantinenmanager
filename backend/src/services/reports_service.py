5000a2ac99a7f265623cdc4da49dfa54a82a7345
Lennox Schuster <fx26ifam@studserv.uni-leipzig.de>
2025-02-04 19:35:18 +0100

    Revert "Merge branch 'revert-f7a6ce1f' into 'develop'"
    This reverts merge request !50

--- backend/src/services/reports_service.py
+++ backend/src/services/reports_service.py
@@ -1,23 +1,8 @@
-from datetime import datetime, timedelta, date, time
+from datetime import datetime, timedelta, time
 import pytz
-from typing import List, Optional, Union
+from typing import List, Union
 from uuid import UUID
-from enum import Enum
-from flask import send_file, Response, make_response
-import csv
-from io import BytesIO, StringIO
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.platypus import (
-    SimpleDocTemplate,
-    Table,
-    TableStyle,
-    Paragraph,
-    Spacer,
-    PageBreak,
-    KeepTogether,
-)
+from flask import Response
 
 from src.repositories.orders_repository import OrdersRepository, OrdersFilters
 from src.repositories.locations_repository import LocationsRepository
@@ -30,89 +15,18 @@ from src.models.dailyorder import DailyOrder
 from src.models.oldorder import OldOrder
 
 from src.schemas.reports_schemas import CountOrdersObject, CountOrdersSchema
-
-from src.utils.exceptions import AccessDeniedError, NotFoundError, BadValueError
-
-
-class OrderType(Enum):
-    OLD_ORDER = OldOrder
-    PRE_ORDER = PreOrder
-    DAILY_ORDER = DailyOrder
-
-
-ORDER_TYPE_GETTER = {
-    "OLD_ORDER": OrdersRepository.get_old_orders,
-    "PRE_ORDER": OrdersRepository.get_pre_orders,
-    "DAILY_ORDER": OrdersRepository.get_all_daily_orders,
-}
+from src.utils.exceptions import AccessDeniedError, NotFoundError, LocationDoesNotExist
+from src.utils.pdf_creator import PDFCreationUtils
+from src.utils.error import ErrMsg, abort_with_err
 
 
 class ReportsService:
 
     @staticmethod
-    def get_printed_report(
-        filters: OrdersFilters,
-        location_ids: Optional[List[UUID]],
-        user_id: UUID,
-        user_group: UserGroup,
-        pdf_bool: bool = True,
-    ) -> Union[Response, None]:
-        """
-        Get a preorders report filterd by date and location(s)
-        :param filters: Filters for old orders
-        :return: a pdf file with the report or None if no orders were found
-        """
-
-        if not location_ids:
-            raise BadValueError("Standort ID fehlt.")
-
-        try:
-            order_types_with_dates = ReportsService._get_order_details(
-                filters.date, filters.date_start, filters.date_end
-            )
-        except BadValueError as err:
-            raise BadValueError(str(err))
-
-        if not order_types_with_dates:
-            raise NotFoundError("Bestellung(en) für das Datum")
-
-        orders: List[PreOrder | DailyOrder | OldOrder] = []
-
-        for order_type, date_start, date_end in order_types_with_dates:
-
-            for location_id in location_ids:
-
-                if ReportsService._check_user_access_to_location:
-                    orders.extend(
-                        ORDER_TYPE_GETTER[order_type](
-                            OrdersFilters(
-                                date_start=date_start,
-                                date_end=date_end,
-                                location_id=location_id,
-                            )
-                        )
-                    )
-
-                else:
-                    raise AccessDeniedError(
-                        f"Nutzer:in hat keine Berechtigung für Standort {location_id}."
-                    )
-
-        location_counts = ReportsService._count_location_orders(orders)
-
-        if pdf_bool:
-            return ReportsService._create_pdf_report(
-                filters=filters, location_counts=location_counts
-            )
-        else:
-            return ReportsService._create_csv_report(
-                filters=filters, location_counts=location_counts
-            )
-
-    @staticmethod
     def get_daily_orders_count(
         user_group: UserGroup, user_id: UUID
     ) -> List[CountOrdersSchema]:
+        """Function for daily_orders_routes"""
 
         if user_group == UserGroup.kuechenpersonal:
             user = UsersRepository.get_user_by_id(user_id)
@@ -138,64 +52,53 @@ class ReportsService:
 
         return CountOrdersSchema(many=True).dump(orders)
 
-    def _get_order_details(
-        single_date: Optional[date],
-        date_start: Optional[date],
-        date_end: Optional[date],
-    ) -> List[dict]:
+    ################################# Reports Service #################################
 
-        timezone = pytz.timezone("Europe/Berlin")
-        today = datetime.now(timezone).date()
-        current_time = datetime.now(timezone).time()
-        yesterday = today - timedelta(days=1)
-        tomorrow = today + timedelta(days=1)
-        before_yesterday = yesterday - timedelta(days=1)
+    @staticmethod
+    def get_location_report(
+        filters: OrdersFilters,
+        user_id: UUID,
+        user_group: UserGroup,
+    ) -> Union[Response, None]:
+        """
+        Create a orders report filterd by date and location
+        :param filters: Filters for date_start, date_end and location_id
+        :return: a pdf file with the report or None if no orders were found
+        """
 
-        order_types = []
+        if not filters.location_id or not filters.date_start or not filters.date_end:
+            raise ValueError("Keine Standort-ID oder Datum übergeben")
 
-        print(single_date, date_start, date_end)
+        orders = ReportsService._get_reports_orders_by_location(
+            fil=filters, user_id=user_id, user_group=user_group
+        )
 
-        if single_date:
+        location_counts = ReportsService._count_location_orders(orders)
 
-            if (single_date == today and current_time <= time(8, 0)) or (
-                single_date > today
+        return PDFCreationUtils.create_pdf_report(
+            filters=filters, location_counts=location_counts
+        )
+
+    def _get_reports_orders_by_location(
+        fil: OrdersFilters,
+        user_id: UUID,
+        user_group: UserGroup,
+    ) -> List[PreOrder | DailyOrder | OldOrder]:
+
+        orders: List[PreOrder | DailyOrder | OldOrder] = []
+
+        if fil.date_start and fil.date_end and (fil.date_start <= fil.date_end):
+            if ReportsService._check_user_access_to_location(
+                fil.location_id, user_id, user_group
             ):
-                order_types.append(("PRE_ORDER", single_date, single_date))
-
-            elif (single_date == today and current_time > time(8, 0)) or (
-                single_date == yesterday and current_time < time(8, 0)
-            ):
-                order_types.append(("DAILY_ORDER", single_date, single_date))
-
+                orders.extend(OrdersRepository.get_pre_orders(fil))
+                orders.extend(OrdersRepository.get_all_daily_orders(fil))
+                orders.extend(OrdersRepository.get_old_orders(fil))
             else:
-                order_types.append(("OLD_ORDER", single_date, single_date))
-
-        elif date_start and date_end and (date_start <= date_end):
-
-            if date_end < yesterday:
-                order_types.append(("OLD_ORDER", date_start, date_end))
-
-            if today < date_start:
-                order_types.append(("PRE_ORDER", date_start, date_end))
-
-            if date_start <= today <= date_end:
-                if current_time < time(8, 0):
-                    order_types.append(("PRE_ORDER", date_start, today))
-                else:
-                    order_types.append(("PRE_ORDER", date_start, tomorrow))
-                    order_types.append(("DAILY_ORDER", today, today))
-
-            if date_start <= yesterday <= date_end:
-                if current_time < time(8, 0):
-                    order_types.append(("DAILY_ORDER", yesterday, yesterday))
-                    order_types.append(("OLD_ORDER", before_yesterday, date_end))
-                else:
-                    order_types.append(("OLD_ORDER", yesterday, date_end))
-
+                raise AccessDeniedError(f"Nutzer:in {user_id}")
         else:
-            raise BadValueError("Kein valides Start- und Enddatum übergeben.")
-
-        return order_types
+            raise ValueError("Kein valides Start- und/oder Enddatum.")
+        return orders
 
     def _check_user_access_to_location(
         location_id: UUID, user_id: UUID, user_group: UserGroup
@@ -242,119 +145,43 @@ class ReportsService:
 
         return location_counts
 
-    def _create_pdf_report(filters: OrdersFilters, location_counts: dict) -> Response:
+    ################################# Invoices Services #################################
 
-        buffer = BytesIO()
-        pdf = SimpleDocTemplate(buffer, pagesize=letter)
-        styles = getSampleStyleSheet()
-        elements = []
+    @staticmethod
+    def get_printed_invoice(
+        filters: OrdersFilters,
+    ) -> Union[Response]:
+        """
+        Get an invoice report #TODO filterd by date and location
+        :param filters: Filters for old orders
+        :return: a pdf file with the report or None if no orders were found
+        """
 
-        date_str = ReportsService._get_date_string(filters)
-        if not date_str:
-            raise BadValueError("Kein Datum übergeben")
-
-        elements.append(Paragraph(f"Datum: {date_str}", styles["Heading2"]))
-        elements.append(Spacer(1, 12))
-
-        for location, counts in location_counts.items():
-            location_block = ReportsService._create_location_pdf_block(
-                location.location_name, counts, styles
-            )
-            elements.append(KeepTogether(location_block))
-
-        pdf.build(elements)
-        buffer.seek(0)
-
-        response = make_response(
-            send_file(
-                buffer,
-                mimetype="application/pdf",
-                as_attachment=True,
-                download_name=f"Report_{date_str}.pdf",
-            )
-        )
-        response.headers["Access-Control-Expose-Headers"] = "Content-Disposition"
-
-        return response
-
-    def _create_location_pdf_block(location_name, counts, styles):
-        location_block = []
-        location_block.append(Paragraph(location_name, styles["Heading3"]))
-        location_block.append(Spacer(1, 8))
-
-        data = [
-            ["Gericht", "Anzahl"],
-            ["Rot", counts["rot"]],
-            ["Blau", counts["blau"]],
-            ["Salat", counts["salad_option"]],
-        ]
-
-        table = Table(data, colWidths=[150, 100])
-        table.setStyle(
-            TableStyle(
-                [
-                    ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
-                    ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-                    ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-                    ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
-                    ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
-                    ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
-                    ("GRID", (0, 0), (-1, -1), 1, colors.black),
-                ]
-            )
-        )
-
-        location_block.append(table)
-        location_block.append(Spacer(1, 20))
-
-        return location_block
-
-    def _create_csv_report(filters: OrdersFilters, location_counts: dict) -> Response:
-
-        buffer = StringIO()
-        csv_writer = csv.writer(buffer)
-
-        date_str = ReportsService._get_date_string(filters)
-        csv_writer.writerow([date_str])
-
-        for location, counts in location_counts.items():
-            ReportsService._write_location_csv(
-                csv_writer, location.location_name, counts
+        if (
+            (filters.person_id and filters.location_id)
+            or (filters.person_id and filters.group_id)
+            or (filters.location_id and filters.group_id)
+        ):
+            raise BadValueError(
+                "Nur eine UUID von Standort, Gruppe ODER Person kann verwendet werden"
             )
 
-        buffer.seek(0)
-        csv_content = buffer.getvalue().encode("utf-8")
+        orders: List[OldOrder] = OrdersRepository.get_old_orders(filters)
 
-        response = make_response(
-            send_file(
-                BytesIO(csv_content),
-                mimetype="text/csv",
-                as_attachment=True,
-                download_name=f"Report_{date_str}.csv",
+        if filters.person_id:
+            return PDFCreationUtils.create_pdf_invoice_person(
+                filters.date_start, filters.date_end, orders, filters.person_id
             )
-        )
-
-        response.headers["Access-Control-Expose-Headers"] = "Content-Disposition"
-
-        return response
-
-    def _write_location_csv(csv_writer, location_name, counts):
-        csv_writer.writerow([])
-        csv_writer.writerow([location_name])
-        csv_writer.writerow(["Gericht", "Anzahl"])
-        csv_writer.writerow(["Rot", counts["rot"]])
-        csv_writer.writerow(["Blau", counts["blau"]])
-        csv_writer.writerow(["Salad", counts["salad_option"]])
-
-    def _get_date_string(filters: OrdersFilters) -> str:
-
-        if filters.date_start and filters.date_end:
-            if filters.date_start.year == filters.date_end.year:
-                date_range = f"{filters.date_start.strftime('%d.%m')}–{filters.date_end.strftime('%d.%m.%Y')}"
-            else:
-                date_range = f"{filters.date_start.strftime('%d.%m.%Y')}–{filters.date_end.strftime('%d.%m.%Y')}"
-            return date_range
-        elif filters.date:
-            return f"{filters.date.strftime('%d.%m.%Y')}"
+        elif filters.location_id:
+            return PDFCreationUtils.create_pdf_invoice_location(
+                filters.date_start, filters.date_end, orders, filters.location_id
+            )
+        elif filters.group_id:
+            return PDFCreationUtils.create_pdf_invoice_group(
+                filters.date_start, filters.date_end, orders, filters.group_id
+            )
         else:
-            return None
+            raise BadValueError(
+                "Keine Standort-ID, Gruppen-ID oder Personen-ID übergeben"
+            )
+            
