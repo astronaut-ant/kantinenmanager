69a21ee46b5984ec5fd4a30e15ff0929dedf62d6
Hazem <hazemrik@gmail.com>
2025-03-03 16:50:48 +0100

    created Footer and adjusted App.vue to include Footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="main-content">
       <router-view />
     </v-main>
+    <Footer></Footer>
   </v-app>
 </template>
 
 <script setup>
 //
 </script>
+
+<style>
+.main-content {
+  min-height: calc(100vh - 64px);
+}
+</style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-footer padless class="footer">
+        <v-row justify="center" no-gutters>
+            <v-btn
+                v-for="link in links"
+                :key="link"
+                class="mx-2"
+                variant="text"
+            >
+                {{ link }}
+            </v-btn>
+            <v-col class="text-center mt-4" cols="12">
+                {{ new Date().getFullYear() }} — <strong>Kantinenmanager Lebenshilfe Borna e.V.</strong>
+            </v-col>
+        </v-row>
+    </v-footer>
+</template>
+
+<script setup>
+const links = ["Impressum", "Datenschutz", "AGB"];
+
+</script>
+
+<style>
+.footer {
+  height: 64px;
+}
+</style>
