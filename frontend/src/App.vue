4dfd39fd5120b2953a859fc87220003beb144294
Hazem <hazemrik@gmail.com>
2025-03-05 23:50:40 +0100

    conditionally render Footer based on route path

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,12 +3,13 @@
     <v-main class="main-content">
       <router-view />
     </v-main>
-    <Footer></Footer>
+    <Footer v-if="useRoute().path !== '/accessdenied'"></Footer>
   </v-app>
 </template>
 
 <script setup>
-//
+import { useRoute } from 'vue-router';
+import Footer from './components/Footer.vue';
 </script>
 
 <style>
