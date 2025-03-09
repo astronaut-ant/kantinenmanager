3db16e2f0ef8206231a65e8902f1af8f02b7a1be
Moritz Oczko <moritz89@icloud.com>
2025-03-09 21:52:20 +0000

    Add daily-orders/own Endpoint (Sidneys Code)

--- backend/src/routes/daily_orders_routes.py
+++ backend/src/routes/daily_orders_routes.py
@@ -117,6 +117,46 @@ def get_daily_orders_counted():
     return jsonify(orders_counted_by_location), 200
 
 
+@daily_orders_routes.get("/api/daily-orders/own")
+@login_required(
+    groups=[
+        UserGroup.gruppenleitung,
+        UserGroup.kuechenpersonal,
+        UserGroup.verwaltung,
+        UserGroup.standortleitung,
+    ]
+)
+@swag_from(
+    {
+        "tags": ["daily_orders"],
+        "responses": {
+            200: {
+                "description": "Returns daily orders for a user",
+                "schema": DailyOrderFullSchema,
+            },
+            404: {"description": "Not found"},
+        },
+    }
+)
+def get_own_daily_order():
+    """
+    Get only our own Daily order as User.
+    """
+    try:
+        daily_order = DailyOrdersService.get_own_daily_order(g.user_id)
+    except NotFoundError as err:
+        abort_with_err(
+            ErrMsg(
+                status_code=404,
+                title="Nicht gefunden",
+                description="Keine Bestellung vorhanden.",
+                details=str(err),
+            )
+        )
+
+    return DailyOrderFullSchema().dump(daily_order), 200
+
+
 # QR-Code scannen
 @daily_orders_routes.get("/api/daily-orders/person/<uuid:person_id>")
 @login_required(groups=[UserGroup.kuechenpersonal])
