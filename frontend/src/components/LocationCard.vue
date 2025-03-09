a68e588e7d630b5399c853ae4fd4757b2afd65a9
Hazem <hazemrik@gmail.com>
2025-03-09 21:01:02 +0100

    add LocationCard component and enhance VerwaltungAlleMitarbeiter with dynamic toolbar

---
+++ frontend/src/components/LocationCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card class="mx-2 my-2" width="425" elevation="16">
+        <v-card-item>
+            <v-card-title>{{ location_name }}</v-card-title>
+            <v-card-subtitle>
+                <v-icon color="primary" icon="mdi-account-circle" size="small"></v-icon>
+                <span class="me-1 ml-2">{{ props.location_leader.first_name }} {{ props.location_leader.last_name }}</span>
+            </v-card-subtitle>
+            <li v-for="group in groups">{{ group }}</li>
+        </v-card-item>
+        <v-card-text>
+            <v-divider></v-divider>
+            <div class="mt-3 d-flex justify-space-between align-center">
+                <v-chip prepend-icon="mdi-account-multiple" color="primary" label density="comfortable"> Gruppenanzahl: {{props.groups.length}} </v-chip>
+                <div class="d-flex ga-1 justify-end">
+                    <v-btn class="bg-primary mx-1" @click="openEditDialog(group)" size="default" density="comfortable"><v-icon>mdi-lead-pencil</v-icon></v-btn>
+                    <v-btn class="bg-red" @click="handleDelete(group)" size="default" density="comfortable"><v-icon>mdi-trash-can-outline</v-icon></v-btn>
+                </div>
+            </div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup>
+const props = defineProps(["id", "location_name", "location_leader", "groups" ])
+
+</script>
--- frontend/src/components/SearchComponents/FilterBar.vue
+++ frontend/src/components/SearchComponents/FilterBar.vue
@@ -1,46 +1,43 @@
 <template>
-    <div class="mt-4">
-      <div v-if="isPinned" class="mt-4" :style="{ height: `${height}px`, width: '100%' }"></div>
-      
-      <div 
-        class="d-flex justify-center align-center sticky-header mt-4"
-        :class="{ pinned: isPinned, transitioning: isTransitioning }"
-        :style="isPinned ? { transform: `translateY(${translateY}px)` } : ''"
-      >
-        <v-toolbar border color="white" density="comfortable" style="max-width: 800px;" rounded="lg" floating>
-          <div v-if="props.viewSwitcherEnabled" class="d-flex align-center flex-column ml-6 mr-6">
-            <v-btn-toggle v-model="ansicht" variant="outlined" base-color="black" color="primary" mandatory divided density="comfortable" @update:modelValue="onToggleChange">
-              <v-btn
-                icon="mdi-card-outline"
-                value="cardview"
-              ></v-btn>
-              <v-btn
-                icon="mdi-table"
-                value="tableview"
-              ></v-btn>
-            </v-btn-toggle>
-          </div>
-          <v-divider v-if="props.viewSwitcherEnabled" inset vertical></v-divider>
-          <v-text-field
-            class="ml-6 mr-6"
-            v-model="search"
-            density="compact"
-            label="Suche"
-            prepend-inner-icon="mdi-magnify"
-            variant="solo-filled"
-            flat
-            hide-details
-            single-line
-            clearable
-            rounded="xl"
-          ></v-text-field>
-        </v-toolbar>
-      </div>
-    </div>
+    <v-sheet color="white" style="position: sticky; top: 0px; z-index: 10; padding-top: 2px; padding-bottom: 2px;" >
+        <div class="d-flex justify-center align-center mt-4" >
+            <v-toolbar border color="white" density="comfortable" style="width: 50%;" rounded="lg" floating >
+            <div v-if="props.viewSwitcherEnabled" class="d-flex align-center flex-column ml-6 mr-6">
+                <v-btn-toggle v-model="ansicht" variant="outlined" base-color="black" color="primary" mandatory divided density="comfortable" @update:modelValue="onToggleChange">
+                <v-btn
+                    icon="mdi-card-outline"
+                    value="cardview"
+                ></v-btn>
+                <v-btn
+                    icon="mdi-table"
+                    value="tableview"
+                ></v-btn>
+                </v-btn-toggle>
+            </div>
+            <v-divider v-if="props.viewSwitcherEnabled" inset vertical></v-divider>
+            <v-text-field
+                class="ml-6 mr-6"
+                v-model="search"
+                density="compact"
+                label="Suche"
+                prepend-inner-icon="mdi-magnify"
+                variant="solo-filled"
+                flat
+                hide-details
+                single-line
+                clearable
+                rounded="xl"
+            ></v-text-field>
+            </v-toolbar>
+        </div>
+        <div class="mt-4 mb-2 d-flex justify-center align-center">
+            <slot name="outside"></slot>
+        </div>
+    </v-sheet>
   </template>
   
   <script setup>
-  import { ref, onMounted, onUnmounted } from "vue";
+  import { ref } from "vue";
 
   const props = defineProps(["items", "viewSwitcherEnabled", "filterList"]);
   const emit = defineEmits(["searchresult", "changeview"]);
@@ -48,43 +45,6 @@
   const ansicht = ref("cardview");
   const search = ref("");
   
-  const isPinned = ref(false);
-  const isTransitioning = ref(false);
-  const translateY = ref(0); 
-  const lastScrollY = ref(window.scrollY);
-  const height = ref(0); 
-  
-  const handleScroll = () => {
-    const currentScrollY = window.scrollY;
-  
-    if (currentScrollY > lastScrollY.value && currentScrollY > height.value) {
-      isPinned.value = true;
-      isTransitioning.value = false;
-      translateY.value = 0;
-    }
-    else if (currentScrollY < lastScrollY.value && currentScrollY > height.value) {
-      isPinned.value = true;
-      isTransitioning.value = false;
-    }
-    else if (currentScrollY < lastScrollY.value && currentScrollY <= height.value) {
-      isPinned.value = false;
-      isTransitioning.value = false;
-      translateY.value = 0;
-    }
-  
-    lastScrollY.value = currentScrollY;
-  };
-  
-  onMounted(() => {
-    const element = document.querySelector(".sticky-header");
-    height.value = element.offsetHeight;
-  
-    window.addEventListener("scroll", handleScroll);
-  });
-  
-  onUnmounted(() => {
-    window.removeEventListener("scroll", handleScroll);
-  });
 
   const normalizedItems = computed(() => Array.isArray(props.items) ? props.items : Object.values(props.items));
 
@@ -110,23 +70,4 @@
     emit('changeview', newValue);
   };
   </script>
-  
-
-<style scoped>
-  .sticky-header {
-    position: relative;
-    z-index: 10;
-    transition: transform 0.3s ease;
-  }
-  
-  .sticky-header.pinned {
-    position: fixed;
-    top: 0;
-    width: 100%;
-  }
-  
-  .sticky-header.transitioning {
-    transition: transform 0.2s ease;
-  }
-</style>
   
