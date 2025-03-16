50b7987b2db008df7464252e5d5d4e8695388748
Sidney Stiller <yi10ihas@uni-leipzig.de>
2025-03-16 17:15:04 +0000

    fix 4 tests and location reports

--- backend/src/services/reports_service.py
+++ backend/src/services/reports_service.py
@@ -115,7 +115,13 @@ class ReportsService:
             ):
                 orders.extend(
                     OrdersRepository.get_pre_orders(
-                        fil, user_group=user_group, user_id=user_id
+                        fil,
+                        user_group=(
+                            UserGroup.verwaltung
+                            if user_group == UserGroup.kuechenpersonal
+                            else user_group
+                        ),
+                        user_id=user_id,
                     )
                 )
                 orders.extend(OrdersRepository.get_all_daily_orders(fil))
@@ -130,14 +136,17 @@ class ReportsService:
         location_id: UUID, user_id: UUID, user_group: UserGroup
     ) -> bool:
 
-        if user_group == UserGroup.verwaltung:
+        if (
+            user_group == UserGroup.verwaltung
+            or user_group == UserGroup.kuechenpersonal
+        ):
             return True
         elif user_group == UserGroup.standortleitung:
             location = LocationsRepository.get_location_by_id(location_id)
-            return user_id == location.user_id_location_leader
+            return str(user_id) == str(location.user_id_location_leader)
         elif user_group == UserGroup.kuechenpersonal:
             user = UsersRepository.get_user_by_id(user_id)
-            return user.location_id == location_id
+            return str(user.location_id) == str(location_id)
         else:
             return False
 
