88d7a7590f5416db15a4596abce794c9c9201d70
Moritz Oczko <moritz89@icloud.com>
2025-03-17 11:36:13 +0100

    responsive NoResult

--- frontend/src/components/SearchComponents/NoResult.vue
+++ frontend/src/components/SearchComponents/NoResult.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="mx-4 d-flex justify-center align-center" style="height:500px">
+  <div class="mx-4 d-flex justify-center align-center">
     <v-card flat class="pa-5">
-      <div class="d-flex align-center justify-center">
+      <div class="d-block d-md-flex align-center justify-center">
         <div>
           <v-img width="150" cover src="@/assets/sad_logo.png"></v-img>
         </div>
         <div>
-          <v-card-title class="text-h5">
-            Keine Ergebnisse gefunden
-          </v-card-title>
-          <v-card-subtitle>
-            Bitte überprüfen Sie Ihre Eingabe auf mögliche Tippfehler oder versuchen Sie es mit anderen Suchbegriffen.
-          </v-card-subtitle>
+          <v-card-title>Keine Ergebnisse gefunden</v-card-title>
+          <v-card-text>
+            Bitte überprüfen Sie Ihre Eingabe auf mögliche Tippfehler oder
+            versuchen Sie es mit anderen Suchbegriffen.
+          </v-card-text>
         </div>
       </div>
     </v-card>
